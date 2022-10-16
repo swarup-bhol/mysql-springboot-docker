519386349f795f212aa864726c26a88d0de4bc3d
swarup-bhol <54322967+swarup-bhol@users.noreply.github.com>
2022-10-16 14:07:32 +0530

    Angular added

--- employe-backend/src/main/java/com/employee/employebackend/EmployeBackendApplication.java
+++ employe-backend/src/main/java/com/employee/employebackend/EmployeBackendApplication.java
@@ -2,12 +2,29 @@ package com.employee.employebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("X-Requested-With", "Content-Type", "Accept", "Origin", "Authorization")
+						.exposedHeaders("")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 
 }
--- employees-frontend/src/app/app.component.ts
+++ employees-frontend/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'employees-frontend';
+  empForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+    
+    ){console.log(1)}
+
+  ngOnInit() {
+    this.empForm = this.fb.group({
+      empId:[''],
+      empName:['']
+    });
+  }
+
+  onSubmit() {
+   this.http.post("http://localhost:8080/insertemployee",this.empForm.value).subscribe(
+    d=>{ alert("employeee created successfully")},
+    e=>{}
+   );
+    console.log(this.empForm.value)
+   alert(this.empForm.value)
+   
+  }
 }
